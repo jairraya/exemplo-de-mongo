a8a6991a546f098d5777e43cb472b8a55360a74f
Bruno Pimentel <bpimentel@mastertech.tech>
2018-07-28 10:44:14 -0300

    Corrigir função de cadastro

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-const cadastros = {
-    'jose': 'jose@jose',
-    'joao': 'joao@joao',
-    'maria': 'maria@maria'
-}
+const cadastros = {};
 
 app.get('/emails', (req, res) => {
     res.send(cadastros);
 });
 
 app.post('/cadastrar', (req, res) => {
-    res.send(req.body);
+    if(!req.body.nome || !req.body.email){
+        return res.status(400).send({mensagem: "Nome e email são obrigatórios"});
+    }
+
+    cadastros[req.body.nome] = req.body.email;
+    res.send({mensagem: 'Cadastro realizado com sucesso!'});
 });
 
 app.listen(3000, () => console.log('Aplicação iniciada.'));
