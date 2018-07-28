3b0bd1a25783e1bca67708b52994b226194740f2
Bruno Pimentel <bpimentel@mastertech.tech>
2018-07-28 11:00:50 -0300

    Adiciona endpoint para busca por nome

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get('/emails', (req, res) => {
     res.send(cadastros);
 });
 
+app.get('/email/:nome', (req, res) => {
+    let email = cadastros[req.params.nome];
+
+    if(!email){
+        return res.status(404).send({mensagem: `Lamentamos, mas não encontramos ${req.params.nome}.`})
+    }
+
+    res.send({'email': email});
+});
+
 app.post('/cadastrar', (req, res) => {
     if(!req.body.nome || !req.body.email){
         return res.status(400).send({mensagem: "Nome e email são obrigatórios"});
