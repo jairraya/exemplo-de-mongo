e9cbd5eef0b94333d22a945e5a867ab15b8000a6
Renan Rodrigo <renanrb@ime.usp.br>
2018-07-30 09:49:39 -0300

    Adicionar endpoint de DELETE

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const expressMongoDb = require('express-mongo-db');
+
+const app = express();
 
 app.use(expressMongoDb('mongodb://localhost/emails'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('static'));
 
-// const cadastros = {};
-
 app.get('/emails', (req, res) => {
-    req.db.collection('cadastros').find({}).toArray((err, cadastros) => {
+    req.db.collection('cadastros').find({}).toArray((erro, cadastros) => {
         res.send(cadastros);
     });
 });
 
 app.get('/email/:nome', (req, res) => {
-    // let email = cadastros[req.params.nome];
-
-    // if(!email){
-    //     return res.status(404).send({mensagem: `Lamentamos, mas não encontramos ${req.params.nome}.`})
-    // }
-
-    req.db.collection('cadastros').findOne({nome: req.params.nome}, (err, cadastro) => {
+    req.db.collection('cadastros').findOne({nome: req.params.nome}, (erro, cadastro) => {
         return res.send(cadastro);
     });
 });
 
-app.post('/cadastrar', (req, res) => {
+app.post('/email/cadastrar', (req, res) => {
     if(!req.body.nome || !req.body.email){
         return res.status(400).send({mensagem: "Nome e email são obrigatórios"});
     }
-
-    // cadastros[req.body.nome] = req.body.email;
-
-    req.db.collection('cadastros').insert(req.body, (err) => {
-        console.log(err);
+    req.db.collection('cadastros').insert(req.body, (erro) => {
+        console.log(erro);
+        return;
     });
 
     res.send({mensagem: 'Cadastro realizado com sucesso!'});
+});
+
+app.delete('/email/:nome', (req, res) => {
+    if(!req.params.nome){
+        return res.status(400).send({mensagem: "Nome é obrigatório"});
+    }
+    req.db.collection('cadastros').deleteOne({nome: req.params.nome}, (erro, cadastro) => {
+        if(erro){
+            console.log(erro);
+            return;
+        }
+        return res.send({mensagem: "Cadastro removido com sucesso!"});
+    });
 });
 
 app.listen(3000, () => console.log('Aplicação iniciada.'));
--- static/scripts.js
+++ static/scripts.js
@@ -3,40 +3,62 @@ let campoEmail = document.querySelector('#email');
 let botao = document.querySelector('button');
 let lista = document.querySelector('ul');
 
+function apagarItem(){
+    let texto = this.previousElementSibling.innerHTML;
+    let nome = texto.split("-")[0];
+    
+    fetch(`http://localhost:3000/email/${nome.trim()}`, {
+    method: 'DELETE'
+}).then((resposta) => {
+    return resposta.json();
+}).then((dados) => {
+    carregar();
+});
+}
+
+function preencherLista(cadastro){
+    let entrada = document.createElement("li");
+    let texto = document.createElement("span");
+    texto.innerHTML = `${cadastro.nome} - ${cadastro.email}`;
+    let botao = document.createElement("button");
+    botao.innerHTML = "Apagar";
+    botao.addEventListener('click', apagarItem);
+    entrada.appendChild(texto);
+    entrada.appendChild(botao);
+    lista.appendChild(entrada);
+}
+
 botao.addEventListener('click', () => {
     let cadastro = {
         nome: campoNome.value,
         email: campoEmail.value
     }
-
+    
     campoNome.value = '';
     campoEmail.value = '';
-
-    fetch('http://10.162.105.176:3000/cadastrar', {
-        method: 'POST',
-        body: JSON.stringify(cadastro),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    }).then((resposta) => {
-        return resposta.json();
-    }).then((dados) => {
-        
-    });
+    
+    fetch('http://localhost:3000/email/cadastrar', {
+    method: 'POST',
+    body: JSON.stringify(cadastro),
+    headers: {
+        'Content-type': 'application/json'
+    }
+}).then((resposta) => {
+    return resposta.json();
+}).then((dados) => {
+    carregar();
+});
 });
 
 function carregar(){
-    fetch('http://10.162.105.176:3000/emails').then((resposta) => {
-        return resposta.json();
-    }).then((cadastros) => {
-        let html = '';
-
-        for(let cadastro of cadastros){
-            html += `<li>${cadastro.nome} - ${cadastro.email}</li>`;
-        }
-
-        lista.innerHTML = html;
-    });
+    fetch('http://localhost:3000/emails').then((resposta) => {
+    return resposta.json();
+}).then((cadastros) => {
+    lista.innerHTML = "";
+    for(let cadastro of cadastros){
+        preencherLista(cadastro);
+    }
+});
 }
 
-setInterval(carregar, 3000);
+carregar();
